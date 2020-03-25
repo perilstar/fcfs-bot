b3e209be859085bafa18c65076baca62d0bdf3b1
Erica DiGiulio <digiulioem@gmail.com>
2020-03-25 12:24:09 -0600

    Finish pingafk command

--- src/commands/pingafk.js
+++ src/commands/pingafk.js
@@ -5,8 +5,7 @@ class PingAfkCommand extends Command {
     super('pingafk', {
       aliases: ['pingafk', 'afk', 'afkcheck'],
       split: 'quoted',
-      channel: 'guild',
-      userPermissions: ['ADMINISTRATOR']
+      channel: 'guild'
     });
   }
 
@@ -18,6 +17,30 @@ class PingAfkCommand extends Command {
       return message.channel.send(`Error: Missing argument: \`mentionUser\`. Use fcfs!help for commands.`);
     }
 
+    let ds = this.client.datasource;
+    let server = ds.servers[message.guild.id];
+    let guild = this.client.guilds.resolve(message.guild.id);
+
+    let voiceState = guild.members.resolve(mention.id).voice;
+
+    if (!voiceState.channelID) {
+      return message.channel.send('Error: User is not in a voice channel');
+    }
+
+    let monitoredChannel = server.monitoredChannels[voiceState.channelID];
+
+    if (!monitoredChannel) {
+      return message.channel.send('Error: User is not in a monitored channel');
+    }
+
+    if (monitoredChannel.restrictedMode) {
+      let sender = message.author;
+      let senderMember = null;
+      if (!senderMember.roles.cache.some(role => monitoredChannel.allowedRoles.includes(role.id))) {
+        return message.channel.send('That user is in a channel which is in Restricted Mode, and your roles don\'t allow you to do this!');
+      }
+    }
+
     let mentionMessage = '**[AFK CHECK]**\nPress thumbs up if you are not AFK to keep your place in the waiting list';
     mention.send(mentionMessage).then(msg => {
       msg.react('👍');
@@ -26,7 +49,7 @@ class PingAfkCommand extends Command {
           return ['👍'].includes(reaction.emoji.name) && user.id === message.author.id;
       };
 
-      msg.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
+      msg.awaitReactions(filter, { max: 1, time: monitoredChannel.afkCheckDuration, errors: ['time'] })
         .then(collected => {
             const reaction = collected.first();
 
@@ -35,13 +58,12 @@ class PingAfkCommand extends Command {
             }
         })
         .catch(collected => {
-          //TODO - remove user from VC queue in text queue
-          
+          voiceState.kick();
 
           msg.reply('You failed to react to the message in time. You have been removed from the queue.');
         });
     });
-
+    message.channel.send('AFK-checking...')
   }
 }
 
