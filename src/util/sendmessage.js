2bf0938151c331a1fdf67c914dd8e0818d6bb166
Erica DiGiulio <digiulioem@gmail.com>
2020-03-28 12:13:41 -0600

    Update error logging

--- src/util/sendmessage.js
+++ src/util/sendmessage.js
@@ -2,7 +2,7 @@ module.exports = (channel, text) => {
   if (channel.deleted) return console.error('Failed to send message in deleted chanenl!');
   channel.send(text).catch(err => {
     if (err.message == 'Missing Permissions') {
-      return console.error(`Failed to send message in ${channel.name} (ID #${channel.id}) due to Missing Permissions!`);
+      return console.error(`Failed to send message in #${channel.name} (ID #${channel.id}) due to Missing Permissions!`);
     }
     throw(err);
   })
